27a94c5b262e99d7a1466a8916712133c76da197
ziani96 <81708016+ziani96@users.noreply.github.com>
2025-03-20 23:59:47 +0000

    login functionality for the backend

--- backend/src/controllers/users-controllers.ts
+++ backend/src/controllers/users-controllers.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
-import UserModel from '../models/users-models';
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import UserModel from "../models/users-models";
+import jwt from "jsonwebtoken";
 
 class UserController {
-
-    public registerUser(req: Request, res: Response): void {
+  public registerUser(req: Request, res: Response): void {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
@@ -14,15 +14,41 @@ class UserController {
     const { username, email, password } = req.body;
 
     UserModel.createUser(username, email, password)
-      .then(user => {
+      .then((user) => {
         res.status(201).json({
-          message: 'User registered successfully',
+          message: "User registered successfully",
           user,
         });
       })
-      .catch(error => {
-        console.error('Error registering user:', error.message);
-        res.status(500).json({ message: 'Server error' });
+      .catch((error) => {
+        console.error("Error registering user:", error.message);
+        res.status(500).json({ message: "Server error" });
+      });
+  }
+
+  public loginUser(req: Request, res: Response): void {
+    const { email, password } = req.body;
+
+    
+
+    UserModel.fetchUserEmailAndConfirmPassword(email, password)
+      .then((user) => {
+        if (!user) {
+          return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign(
+          { id: user.id, email: user.email },
+          "your_secret_key",
+          { expiresIn: "1h" }
+        );
+
+        res.status(200).send({ message: "Login successful", token });
+      })
+      .catch((error) => {
+        console.error("Login error:", error);
+        res.status(500).json({ message: "Server error" });
       });
   }
 }
