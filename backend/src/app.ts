539429804e3909ab3157c9fcd9c24ed614fd64e6
CaptOrb <tehorb13@gmail.com>
2025-03-20 13:32:09 +0000

    fix structure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,11 @@
 import express from 'express';
 import usersRouter from './routes/users-routes';
-import { config } from './config/env';
-import { connectDB } from './config/db';
+
 const app = express();
 
-app.use('/users', usersRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const startServer = async () => {
-  await connectDB();  // Connect to DB before starting the app
-  app.listen(config.port, () =>
-    console.log(`Server is running on port ${config.port}`));
+app.use('/users', usersRouter);
 
-};
-
-startServer();
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import { config } from './config/env'; 
+import { connectDB } from './config/db';
+import app from './app';
+
+const startServer = async () => {
+    await connectDB();  // Connect to DB before starting the app
+    app.listen(config.port, () =>
+        console.log(`Server is running on port ${config.port}`));
+
+  };
+  
+  startServer(); 
