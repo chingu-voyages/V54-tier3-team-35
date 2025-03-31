fa458f7a7b26a8072c27da3a8fcb19e430473d15
= <=>
2025-03-31 18:44:18 +0100

    handles history fetch errros

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -17,7 +17,13 @@ export default function ContributionForm() {
   const [result, setResult] = useState("11111");
   const [isResultFocused, setIsResultFocused] = useState(false);
   const navigate = useNavigate();
-  const [userHistory, setUserHistory] = useState([ {
+
+  interface HistoryItem {
+    title: string;
+    result: string;
+  }
+
+  const historyMock = [{
     title: "Frontend Developer Prompt1",
     result: "You are a junior frontend developer eager to build your first portfolio.",
   },
@@ -32,7 +38,9 @@ export default function ContributionForm() {
   {
     title: "Frontend Developer Prompt4",
     result: "You are a junior frontend developer eager to build your first portfolio.",
-  },]);
+  }];
+
+  const [userHistory, setUserHistory] = useState<HistoryItem[]>([]);
   const [viewingHistory, setViewingHistory] = useState(false);
   const [resultTitle, setResultTitle] = useState("Result");
   const [formData, setFormData] = useState(initialFormState);
@@ -41,9 +49,13 @@ export default function ContributionForm() {
     const fetchHistory = async () => {
       try {
         const response = await axios.get(`${API_URL}/history`);
-        setUserHistory(response.data.history);
-      } catch (error) {
-        console.error("Failed to fetch user history:", error);
+        setUserHistory(response?.data?.history || [...historyMock]); // temporal while not querying the query history
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("Failed to fetch user history:", error.message);
+        } else {
+          console.error("Failed to fetch user history:", error);
+        }
       }
     };
 
