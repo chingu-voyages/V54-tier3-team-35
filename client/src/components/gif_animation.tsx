55987691052941fd149227ee459511b382f91dbf
Ahmed Sohail <ahmedsohail02000@gmail.com>
2025-04-01 18:06:06 +0500

    changed file again

--- client/src/components/gif_animation.tsx
+++ client/src/components/gif_animation.tsx
@@ -1,11 +1,14 @@
-// client/src/components/GifAnimation.tsx
 import React from 'react';
 
 const GifAnimation: React.FC = () => {
   return (
     <div className="gif-container">
-      {/* Correct path to the gif in the public folder */}
-      <img src="/gif_animation.gif" alt="App Demo" className="responsive-gif" />
+      <img
+        src="/gif_animation.gif"
+        alt="App Demo"
+        className="responsive-gif"
+        style={{ maxWidth: '100%', height: 'auto' }}  // Resize while maintaining aspect ratio
+      />
     </div>
   );
 };
