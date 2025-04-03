9809962da52e7b02253c3f9dfebe6a466885c1eb
ziani96 <81708016+ziani96@users.noreply.github.com>
2025-04-03 22:29:59 +0100

    styling changes to sidebar and mainpage

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContributionForm from "../components/ContributionForm/ContributionForm";
+import Footer from "../components/Footer";
 
 const MainPage: React.FC = () => {
   return (
     <div>
       <ContributionForm />
+      <Footer />
     </div>
   );
 };
