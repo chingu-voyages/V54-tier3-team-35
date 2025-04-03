e93a625537956f44e41154a728218fd3a63435fc
CaptOrb <tehorb13@gmail.com>
2025-04-03 16:41:12 +0100

    minor fix

--- backend/src/middleware/users-middleware.ts
+++ backend/src/middleware/users-middleware.ts
@@ -43,6 +43,7 @@ class UserMiddleware {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
        res.status(400).json({ errors: errors.array() });
+       return;
     }
     next();
   }
