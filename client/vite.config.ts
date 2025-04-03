25b78b68b5fc036847d3cf30565c23448c3f6eee
CaptOrb <tehorb13@gmail.com>
2025-04-03 01:21:01 +0100

    production prep continued

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
-
 export default defineConfig({
   plugins: [react()],
   base: './',
@@ -10,6 +8,14 @@ export default defineConfig({
     outDir: './dist',
   },
   server: {
-    port: 5173
-    }
+    port: 5173,
+    host: '127.0.0.1',  
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      },
+    },
+  },
 });
