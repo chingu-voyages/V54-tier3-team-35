6d27b58170cbfcfb32fe31795f36c8b3fcfdcb69
Conor Barry <conor_barry@icloud.com>
2025-04-07 15:11:24 +0100

    Revert "developmet cors update"

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 export const config = {
     port: process.env.PORT || 3000,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',
     GEMINI_MODEL: process.env.GEMINI_MODEL || 'gemini-1.5-flash',
-    CLIENT_URL: process.env.CLIENT_URL || 'https://staging-askiq.netlify.app/',
+    CLIENT_URL: process.env.CLIENT_URL || 'https://askiq-ai.netlify.app',
     SECRET_KEY: process.env.SECRET_KEY
   };
