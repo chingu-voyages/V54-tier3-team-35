8e5ef752702c6728a9942939788f9f826439fd3c
= <=>
2025-04-07 13:35:12 +0100

    developmet cors update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,9 +11,13 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      config.CLIENT_URL,
+      // "http://localhost:3000",
+      // "http://localhost:5173",
+      // "https://deploy-preview-88--staging-askiq.netlify.app/",
+      // "https://staging-askiq.netlify.app/",
+      // "https://askiq-live.netlify.app/",
+      "*",
+      config.CLIENT_URL
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
@@ -22,6 +26,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 if (process.env.NODE_ENV !== "production") {
   import("./swagger")
     .then(({ setupSwagger }) => {
