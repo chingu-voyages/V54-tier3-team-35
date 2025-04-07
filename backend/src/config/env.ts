b8700d895e42d6f45921e7965d9eeb331af41d56
= <=>
2025-04-07 13:35:12 +0100

    developmet cors update

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,6 +2,6 @@ export const config = {
     port: process.env.PORT || 3000,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',
     GEMINI_MODEL: process.env.GEMINI_MODEL || 'gemini-1.5-flash',
-    CLIENT_URL: process.env.CLIENT_URL || 'https://askiq-ai.netlify.app',
+    CLIENT_URL: process.env.CLIENT_URL || 'https://staging-askiq.netlify.app/',
     SECRET_KEY: process.env.SECRET_KEY
   };
